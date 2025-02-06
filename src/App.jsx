99af48dde5385bf9fee3e3bbdf8f7bdc89a7ae2a
Fenyyx <fenixg12@gmail.com>
2025-02-06 15:43:03 +0000

    Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import "./App.css";
+import React from 'react';
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Importo las rutas que ya están instaladas
 
 function App() {
-
   return (
-    <div className="App relative z-20 pt-20">
-      <Navbar />
+    
+      <div className="App relative z-20 pt-20">
+        <Navbar />
+        
+        <div className="pages">
+          
+          <Routes>
+            {/* Ruta para HomePage */}
+            <Route path="/" element={<HomePage />} />
 
-      <div className="pages">
-        <HomePage />
+            {/* Ruta para StudentDetailsPage con studentId */}
+            <Route path="/students/:studentId" element={<StudentDetailsPage />} />
 
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+            {/* Ruta para UserProfilePage */}
+            <Route path="/profile" element={<UserProfilePage />} />
+          </Routes>
+        </div>
+        
       </div>
-
-    </div>
+    
   );
 }
 
 export default App;
+
